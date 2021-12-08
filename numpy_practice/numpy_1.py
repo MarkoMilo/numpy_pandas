32671b9e2973f1fc8d6242e26d9fd7f82577f1b7
MarkoM <marko.milosavljevic@ucircuit.com>
2021-12-08 15:51:26 +0100

    update

--- numpy_practice/numpy_1.py
+++ numpy_practice/numpy_1.py
@@ -10,10 +10,244 @@ for i in red:
     print("array: ", arr)
     print("Dimension of array: ", arr.ndim)
 
+# Numpy data types
+arr = np.array([1, 2, 3, 4])
+print(arr.dtype)
+
+arr = np.array(['apple', 'banana', 'cherry'])
+print(arr.dtype)
+
+arr = np.array([1, 2, 3, 4], dtype='S')
+print(arr)
+print(arr.dtype)
+
+arr = np.array([1, 2, 3, 4], dtype='i2')
+print(arr)
+print(arr.dtype)
+
+arr = np.array([1.1, 2.1, 3.1])
+print(arr)
+print(arr.dtype)
+newarr = arr.astype(int)
+print(newarr)
+print(newarr.dtype)
+
+# NUMPY ARRAY COPY VS VIEW
+# THE MAIN DIFFERENCE BETWEEN A COPY AND A VIEW OF AN ARRAY IS THAT THE COPY IS A NEW ARRAY, AND THE VIEW IS JUST A
+# VIEW OF THE ORIGINAL ARRAY.
+
+arr = np.array([1, 2, 3, 4, 5])
+y = arr.copy()
+arr[0] = 42
+print(arr)
+print(y)
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+arr[0] = 42
+print(arr)
+print(x)
+
+arr = np.array([1, 2, 3, 4, 5])
+z = arr.view()
+z[0] = 31
+print(arr)
+print(x)
+
+# Check is array owns it's data
+print("\n*************************************\n")
+print(arr.base)
+print(z.base)
+print(y.base)
+
+# NUMPY ARRAY SHAPE - tHE NUMBER OF ELEMENTS IN EACH DIMENSION
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8, ]])
+print("tHE NUMBER OF ELEMENTS IN EACH DIMENSION", arr.shape)
+
+arr = np.array([1, 2, 3, 4], ndmin=5)
+print(arr)
+print('shape of array :', arr.shape)
+
+
+# NUMPY ARRAY RESHAPING
+# THE SHAPE OF AN ARRAY IS THE NUMBER OF ELEMENTS IN EACH DIMENSION.
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(4, 3)
+print("/////////////\n",newarr)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+newarr = arr.reshape(2, 3, 2)
+print(newarr)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+x = arr.reshape(2, 4).base
+print(x, type(x))
+
+y = arr.copy()
+yy = arr.reshape(2, 4).base
+print(yy, type(yy))
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+newarr = arr.reshape(-1, 2, 2)
+print(newarr)
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
+print("******\n",newarr)
+
+
+"""
+* NUMPY ARRAY ITERATING
+
+ITERATING MEANS GOING THROUGH ELEMENTS ONE BY ONE.
+AS WE DEAL WITH MULTI-DIMENSIONAL ARRAYS IN NUMPY, WE CAN DO THIS USING BASIC FOR LOOP OF PYTHON.
+IF WE ITERATE ON A 1-D ARRAY IT WILL GO THROUGH EACH ELEMENT ONE BY ONE.
+"""
+
+arr = np.array([1, 2, 3])
+for x in arr:
+  print(x)
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+for x in arr:
+  print(x)
+
+# Iterating through 3-D array
+print("# Iterating through 3-D array\n")
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+for x in arr:
+    print(x)
+    for y in x:
+        print(y)
+        for z in y:
+            print(z)
+
+
+# nditer()
+print("\n\nnditer()\n")
+
+arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+for i in np.nditer(arr):
+    print("i is: ", i)
+
+arr = np.array([1, 2, 3])
+for x in np.nditer(arr, flags=['buffered'], op_dtypes=['S']):
+  print("********", x)
+
+print("Iterate through every second element using nditer ")
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for i in np.nditer(arr[:, ::2]):
+    print(i)
+
+print("Enumerated Iteration Using ndenumerate(), first element in for is element position, the second is element")
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for i, j in np.ndenumerate(arr):
+    print("{}\n{}".format(i, j))
+
+"""
+NUMPY JOINING ARRAY
+JOINING MEANS PUTTING CONTENTS OF TWO OR MORE ARRAYS IN A SINGLE ARRAY.
+IN SQL WE JOIN TABLES BASED ON A KEY, WHEREAS IN NUMPY WE JOIN ARRAYS BY AXES.
+"""
+
+print("NUMPY JOINING ARRAY")
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))
+print(arr)
+
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+arr = np.concatenate((arr1, arr2), axis=1)
+print(arr)
+
+print("\n***************************\n")
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
+
+print("\n***************************\n")
+arr1 = np.array([[1, 2], [3, 4]])
+arr2 = np.array([[5, 6], [7, 8]])
+arr = np.stack((arr1, arr2), axis=1)
+print(arr)
+
+print("\n***************************\n")
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.hstack((arr1, arr2))
+print(arr)
+
+print("\n***************************\n")
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.vstack((arr1, arr2))
+print(arr)
+
+print("\n***************************\n")
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr3 = np.array([7, 8, 9])
+arr = np.dstack((arr1, arr2, arr3))
+print(arr)
+
+"""
+Splitting NumPy Arrays
+We use array_split() for splitting arrays, we pass it the array we want to split and the number of splits.
+"""
+
+print("\n***************************\n")
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+print("\n***************************\n")
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 4)
+print(newarr)
+
+
+print("\n***************************\n")
+arr = np.array([1, 2, 3, 4, 5, 6])
+newarr = np.array_split(arr, 3)
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+print("\n Split 2-D arrays ***************************\n")
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+
+
+print("\n***********************************************************\n")
+print("\n Split the 2-D array into three 2-D arrays. \n")
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+print("\n***********************************************************")
+print("Split the 2-D array into three 2-D arrays along rows.. \n")
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.array_split(arr, 3, axis=1)
+print(newarr)
+
+print("\n***********************************************************")
+print("Use the hsplit() method to split the 2-D array into three 2-D arrays along rows. \n")
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15], [16, 17, 18]])
+newarr = np.hsplit(arr, 3)
+print(newarr)
+
 arr = np.array([1,2,3], ndmin=5)
 print(arr)
 print('number of dimensions :', arr.ndim)
-
 
 arr = np.array([1, 2, 3, 4])
 print(arr[2] + arr[3])
--- numpy_practice/pandas_examples.py
+++ numpy_practice/pandas_examples.py
@@ -1,11 +1,170 @@
 import pandas as pd
 
 mydataset = {
-  'cars': ["BMW", "Volvo", "Ford"],
-  'passings': [3, 7, 2]
+    'cars': ["BMW", "Volvo", "Ford"],
+    'passings': [3, 7, 2]
 }
 
-print("dataset is: {}\ntype of dataset is: {}".format(mydataset, type(mydataset)))
+print("dataset is: \n{}\ntype of dataset is: {}".format(mydataset, type(mydataset)))
 
 myvar = pd.DataFrame(mydataset)
 print("dataset is: \n{}\ntype of dataset is: {}".format(myvar, type(myvar)))
+
+# PANDAS SERIES
+print("\n***********************************************************")
+print("Create simple Pandas series from list \n")
+a = [1, 7, 2]
+myvar = pd.Series(a)
+print(myvar)
+print(myvar[0])
+
+print("\n***********************************************************")
+print("Create simple Pandas series from list with own labels \n")
+a = [1, 7, 2]
+print("tip", type(a[0]))
+myvar = pd.Series(a, index=['x', 'y', 'z'])
+print(myvar)
+print(myvar['y'])
+
+print("\n***********************************************************")
+print("Dict in series \n")
+calories = {"day1": 420, "day2": 380, "day3": 390}
+myvar = pd.Series(calories)
+print(myvar)
+print('Create a Series using only data from "day1" and "day2" \n')
+myvar = pd.Series(calories, index=["day1", "day2"])
+print(myvar)
+
+print("\n***********************************************************")
+print("Data frames from two series \n")
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index=['x', 'y', 'z'])
+print(df)
+
+df = pd.DataFrame(data)
+print(df)
+
+print("\n***********************************************************")
+print("Print first row from Data Frame \n")
+print(df.loc[0])
+print("type: ", type(df.loc[0]))
+
+print("\n***********************************************************")
+print("Print first and second row from Data Frame \n")
+print("Data: ", df.loc[[0, 1]])
+
+# PANDAS READ CSV
+
+pd.options.display.max_rows = 60  # defone max number of displayed rows in dataframe
+df = pd.read_csv("generic-food.csv")
+print(df)
+print(pd.options.display.max_rows)
+
+print("\n***********************************************************")
+print(" \n")
+df = pd.read_csv("generic-food.csv")
+print(df)
+
+print("\n***********************************************************")
+print("Read json or json file \n")
+import pandas as pd
+
+data = {
+  "Duration":{
+    "0":60,
+    "1":60,
+    "2":60,
+    "3":45,
+    "4":45,
+    "5":60
+  },
+  "Pulse":{
+    "0":110,
+    "1":117,
+    "2":103,
+    "3":109,
+    "4":117,
+    "5":102
+  },
+  "Maxpulse":{
+    "0":130,
+    "1":145,
+    "2":135,
+    "3":175,
+    "4":148,
+    "5":127
+  },
+  "Calories":{
+    "0":409,
+    "1":479,
+    "2":340,
+    "3":282,
+    "4":406,
+    "5":300
+  }
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+print(df.info())
+
+
+df.to_csv(r'data.csv', index=False, header=True)
+
+df = pd.read_csv("generic-food.csv")
+print(df.info())
+
+# PANDAS - CLEANING EMPTY CELLS
+print("\n***********************************************************")
+print("Pandas - Cleaning Empty Cells \n")
+df = pd.read_csv("data1.csv")
+print(df)
+
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+# print("\n***********************************************************")
+# print(" \n")
+#
+

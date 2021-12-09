a5641225353c458b8c2cc4cfc272d8d03fbaf51a
MarkoM <marko.milosavljevic@ucircuit.com>
2021-12-09 20:54:47 +0100

    update

--- numpy_practice/pandas_examples.py
+++ numpy_practice/pandas_examples.py
@@ -1,5 +1,22 @@
 import pandas as pd
 
+"""
+USEFULL PANDAS METHODS
+import panadas as pd - pd shortcat uses as pandas
+
+df = pd.series(<data>) Load data into pandas series
+df = pd.DataFrame(<data>)
+  <data> could be 'dictionari', 'list', 'nested list', 'tuple' 
+df.loc[<row index>] - show indexed row
+df = pd.read_csv("<csv file>") - read csv file to DataFrame
+df.to_string() - print entire DataFrame
+pd.options.display.max_rows - check system maximum rows to display DataFrame
+pd.options.display.max_rows = 9999 - set system maximum rows to display DataFrame
+df1 = pd.read_json('<file.json>') - load json file into DataFrame
+df1.to_csv("<filename.csv>") - write json to csv file 
+
+"""
+
 mydataset = {
     'cars': ["BMW", "Volvo", "Ford"],
     'passings': [3, 7, 2]
--- numpy_practice/practice.py
+++ numpy_practice/practice.py
@@ -1,13 +1,19 @@
-try:
-    f = open("demofile.txt", mode='a')
-    try:
-        f.write("Marko M\n")
-    except:
-        print("Problem when writting to the file")
-    finally:
-        f.close()
-except:
-    print("Problem with opening file")
-finally:
-    f = open("demofile.txt", "a+")
-    f.close()
+# try:
+#     f = open("demofile.txt", mode='a')
+#     try:
+#         f.write("Marko M\n")
+#     except:
+#         print("Problem when writting to the file")
+#     finally:
+#         f.close()
+# except:
+#     print("Problem with opening file")
+# finally:
+#     f = open("demofile.txt", "a+")
+#     f.close()
+
+import pandas as pd
+
+a = [[1, 3 ,5 , 6], [3, 4, 5, 6]]
+df = pd.Series(a)
+print(df[0])
